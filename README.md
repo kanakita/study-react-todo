968b865a85ffb7fd3e3425afebc1a453132c0c6c
kana.kita <kitagawa@ham.works>
2021-10-06 18:32:31 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # study-react-todo
+
+ReactでTODOアプリを作る
---
+++ src/App.js
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <div className="row">
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+      <div className="col-sm">
+        One of three columns
+      </div>
+    </div>
+  );
+
+}
+
+
+function App() {
+  return <Header />
+}
+
+export default App;
